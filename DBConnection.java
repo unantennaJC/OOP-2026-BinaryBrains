6de471939b6b70020f62c4c3562bede8de4bbc21
Shehani2005 <shehaniliyanage5@gmail.com>
2026-06-08 01:44:38 +0530

    Inside database package

---
+++ DBConnection.java
@@ -0,0 +1,33 @@
+package com.greenloop.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/greenloop";
+
+    private static final String USER =
+            "root";
+
+    private static final String PASSWORD =
+            "Shehani@2005";
+
+    public static Connection getConnection() {
+
+        try {
+
+            return DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
