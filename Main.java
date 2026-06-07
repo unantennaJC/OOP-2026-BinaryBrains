b74adeaef8ba348733c24e314a0bbad74b4e28c4
Shehani2005 <shehaniliyanage5@gmail.com>
2026-06-07 22:10:34 +0530

    Add Main class to launch OrderManagementUI

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.greenloop;
+
+import com.greenloop.ui.OrderManagementUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        OrderManagementUI ui =
+                new OrderManagementUI();
+
+        ui.setVisible(true);
+    }
+}
