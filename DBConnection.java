dde41811e1ecbabf4102813e2b636693bbcd4cbc
VidushaCS <vidushasriyananda@gmail.com>
2026-06-08 11:01:11 +0530

    Add files via upload

--- DBConnection.java
+++ DBConnection.java
@@ -2,18 +2,14 @@ package com.greenloop.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/greenloop";
     private static final String USER = "root";
-    private static final String PASSWORD = "Shehani@2005";
+    private static final String PASSWORD = "Vcs123@#$";
 
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
