7828cc346c50f044103ef87486607a2475b05fe0
VidushaCS <vidushasriyananda@gmail.com>
2026-06-08 10:29:25 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,28 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/greenloop";
-
-    private static final String USER =
-            "root";
-
-    private static final String PASSWORD =
-            "Shehani@2005";
+    private static final String URL = "jdbc:mysql://localhost:3306/greenloop";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Shehani@2005";
 
     public static Connection getConnection() {
-
         try {
-
-            return DriverManager.getConnection(
-                    URL,
-                    USER,
-                    PASSWORD
-            );
-
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
-
             e.printStackTrace();
             return null;
         }
